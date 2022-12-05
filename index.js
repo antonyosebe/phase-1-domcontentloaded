4c44655409687c4969f714cf623649f1399029cc
antonyosebe <osebe.anto@gmail.com>
2022-12-05 07:52:02 +0300

    commit tree e11509f487d19fc222cb192b6d7ca25261a3004f

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("The DOM has loaded");
+    let text = document.getElementaryByid("text");
+    text.innerHtml = "JavaScript is so cool. It lets me add text to my page programmatically."
+});
+
